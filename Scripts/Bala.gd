f594cd2b769c502332e3b5844e1a3887fe39fb52
unknown <tomybecher@hotmail.com>
2020-05-12 07:39:21 -0300

    Actualizacion detalles 12/5

--- Scripts/Bala.gd
+++ Scripts/Bala.gd
@@ -1,76 +1,63 @@
 extends RigidBody2D
 
-#var dir = 1
-#var speed = 10
-#const grav = 2
-
 enum {
 	ESTADO_QUIETO,
-	ESTADO_TRANSFERIR,
-	ESTADO_TOMADO,
 	ESTADO_ARRASTRADO,
 	ESTADO_RELEASED,
 	ESTADO_LANZADO
 	}
-
+	
+var disparo = true
 var estado = ESTADO_QUIETO
-var velocidad_lineal
-var pos_ini = position
-var dp 
-var impulso
+var dif_de_posicion = position - get_global_mouse_position()
+var impulso = null
 
 func _integrate_forces(state):
-	
-#	var velocidad_ang = state.angular_velocity
-#	var velocidad_lineal = state.linear_velocity
-	var delta = 1 / state.step
-	dp = position - pos_ini
-	impulso = dp *0.06
-#	print(velocidad_lineal * delta)
-#	var diferencia_de_posicion = position - get_global_position()
-#	match estado:
-#		ESTADO_TRANSFERIR:
-#			pass
-#		ESTADO_TOMADO:
-#			pass
-#		ESTADO_ARRASTRADO:
-#			var fuerza = get_global_mouse_position() - position
-#			fuerza = fuerza.clamped(100)
-#			velocidad_lineal = (fuerza + diferencia_de_posicion) * delta
-#		ESTADO_RELEASED:
-#			pass
-#		ESTADO_LANZADO:
-#			pass
-	
-	if dp.length() < impulso.length() / delta:
-		pass
+	if (estado != ESTADO_QUIETO):
+		var velocidad_ang = state.angular_velocity
+		var velocidad_lineal = state.linear_velocity
+		var delta = 1 / state.step
+		dif_de_posicion = position - get_global_mouse_position()
+		if Input.is_action_just_released("lefft_mouse") and disparo:
+			disparo = false
+			visible = true
+			estado = ESTADO_RELEASED
+			impulso = dif_de_posicion *0.06 #obtener_impulso()# diferencia_de_posicion *0.06
+		match estado:
+			ESTADO_ARRASTRADO:
+				var fuerza = get_global_mouse_position() - position
+				velocidad_lineal = (fuerza + dif_de_posicion) * delta
+			ESTADO_RELEASED:
+				if dif_de_posicion.length() < impulso.length():
+					estado = ESTADO_LANZADO
+				else:
+					velocidad_lineal = impulso * delta
+			ESTADO_LANZADO:
+				pass 
+		state.linear_velocity = velocidad_lineal
+		state.angular_velocity = velocidad_ang
 	else:
-		state.linear_velocity = velocidad_lineal * delta
-	#state.angular_velocity = velocidad_ang
-	
+		set_can_sleep(true)
 
 
+func _input(event):
+	if event.is_action_pressed("lefft_mouse") and disparo:
+		set_can_sleep(false)
+		estado = ESTADO_ARRASTRADO
 
+
+# PARTE EXPLOSION BALA
 
 var explosion = preload("res://Escenas/explosion.tscn")
 
-
 func bang():
 	var bang = explosion.instance()
-	print("entro a bang")
 	bang.position = global_position
 	get_tree().get_root().add_child(bang)
 	queue_free()
 		
-
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
-
-#func _process(delta):
-	#var move = move_and_collide(Vector2(dir,speed*delta*grav))
-	
-#	if move != null:  #SIEMPRE QUE MOVE SEA != NULL ES PORQUE COLISIONO ENTONCES BORRO
-#		self.queue_free()  
 
 func _on_Bala_body_entered(body):
 	bang()
